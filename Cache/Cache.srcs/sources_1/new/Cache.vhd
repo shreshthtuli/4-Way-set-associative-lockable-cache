95ded343d6e69ebb42f81700ac3c41f7730d9054
Shreshth Tuli <shreshthtuli@gmail.com>
2018-09-18 21:05:39 +0530

    created vivado project

---
+++ Cache/Cache.srcs/sources_1/new/Cache.vhd
@@ -0,0 +1,28 @@
+----------------------------------------------------------------------------------
+-- Company: Indian Institute of technology Delhi
+-- Engineer: Shreshth Tuli
+-- 
+-- Create Date: 09/18/2018 09:02:42 PM
+-- Design Name: 4 way set associative cache
+-- Module Name: Cache - Behavioral
+-- Project Name: Assignment 2 COL719
+
+-- Revision 0.01 - File Created
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.CONV_STD_LOGIC_VECTOR;
+use IEEE.NUMERIC_STD.ALL;
+
+entity Cache is
+--  Port ( );
+end Cache;
+
+architecture Behavioral of Cache is
+
+begin
+
+
+end Behavioral;
